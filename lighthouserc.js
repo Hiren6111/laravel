e522bfb4ba8e25e22b51dd57d3afe5a1f73e7285
Hiren6111 <64456105+Hiren6111@users.noreply.github.com>
2021-06-24 17:58:32 +0530

    Update lighthouserc.js

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,7 +2,7 @@ module.exports = {
   ci: {
     collect: {
       url: '[http://127.0.0.1:8000/'],
-        startServerCommand: 'rails server -e production',
+        startServerCommand: 'php artisan serve',
     },
     upload: {
       target: 'temporary-public-storage',
